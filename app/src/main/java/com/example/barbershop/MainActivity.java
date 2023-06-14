1af5c3f892067a1058eea555d0c426cb352699b6
michaelhung123 <zuroken140@gmail.com>
2023-06-14 13:03:54 +0700

    UI menu + login

---
+++ app/src/main/java/com/example/barbershop/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.barbershop;
+
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+}
---
+++ app/src/main/java/com/example/barbershop/sldsmd/md.java
@@ -0,0 +1,4 @@
+package com.example.barbershop.sldsmd;
+
+public class md {
+}
